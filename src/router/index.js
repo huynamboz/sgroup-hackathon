7120fda5cad00ed6a2e41c70341d1694f9b61707
huynamboz <teahuynam2003@gmail.com>
2023-09-23 00:14:53 +0700

    init src

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: () => import('@/views/Dashboard.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/users',
+    name: 'users',
+    meta: { requiresAuth: true, roles: ['ADMIN'] },
+    component: () => import('@/views/UserList.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import('@/views/auth/LoginPage.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import('@/views/auth/RegisterPage.vue'),
+  },
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
